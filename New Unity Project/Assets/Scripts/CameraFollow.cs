19ffbb8ddf2e9cbaa56bc02d62fbb220b1a98239
Jacob Molnia <maxmustermann@Jakes-Mac.local>
2021-11-08 15:04:39 +0100

    Tag1

---
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public double smoothSpeed = 0.1; 
+    public Vector3 offset; 
+
+    void LateUpdate() {
+
+        transform.position = target.position + offset; 
+    }
+
+}
